af98980489b39991c9b6fc3d5cca438b96fcb9ab
Jonathan Dance (JD) <wuputah@users.noreply.github.com>
2023-06-28 13:22:48 -0500

    version of `analyze.js` that creates bencher JSON (#3)
    * version of `analyze.js` that creates bencher JSON
      also takes a prefix as cmdline arg
    * update gitignore
    * update warehouse \copy paths
    * fix regexps

--- warehouse/data.sql
+++ warehouse/data.sql
@@ -1,17 +1,9 @@
-\copy customer FROM 'tpc-h/data/customer.csv' WITH (FORMAT csv, DELIMITER '|');
-
-\copy lineitem FROM 'tpc-h/data/lineitem.csv' WITH (FORMAT csv, DELIMITER '|');
-
-\copy nation FROM 'tpc-h/data/nation.csv' WITH (FORMAT csv, DELIMITER '|');
-
-\copy orders FROM 'tpc-h/data/orders.csv' WITH (FORMAT csv, DELIMITER '|');
-
-\copy part FROM 'tpc-h/data/part.csv' WITH (FORMAT csv, DELIMITER '|');
-
-\copy partsupp FROM 'tpc-h/data/partsupp.csv' WITH (FORMAT csv, DELIMITER '|');
-
-\copy region FROM 'tpc-h/data/region.csv' WITH (FORMAT csv, DELIMITER '|');
-
-\copy supplier FROM 'tpc-h/data/supplier.csv' WITH (FORMAT csv, DELIMITER '|');
-
+\copy customer FROM 'warehouse/data/customer.csv' WITH (FORMAT csv, DELIMITER '|');
+\copy lineitem FROM 'warehouse/data/lineitem.csv' WITH (FORMAT csv, DELIMITER '|');
+\copy nation FROM 'warehouse/data/nation.csv' WITH (FORMAT csv, DELIMITER '|');
+\copy orders FROM 'warehouse/data/orders.csv' WITH (FORMAT csv, DELIMITER '|');
+\copy part FROM 'warehouse/data/part.csv' WITH (FORMAT csv, DELIMITER '|');
+\copy partsupp FROM 'warehouse/data/partsupp.csv' WITH (FORMAT csv, DELIMITER '|');
+\copy region FROM 'warehouse/data/region.csv' WITH (FORMAT csv, DELIMITER '|');
+\copy supplier FROM 'warehouse/data/supplier.csv' WITH (FORMAT csv, DELIMITER '|');
 ANALYZE;
